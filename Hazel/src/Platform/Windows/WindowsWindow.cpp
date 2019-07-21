837a5327edbcabc543ea936ba4bca7c119c1c49e
Svetlozar Georgiev <s.georgiev255@gmail.com>
2019-07-21 12:07:31 +0100

    fixed linux build failing by specifying OpenGL version explicitly; this is probably good practice anyway
    without specifying a version, version 3.3 was being used which resulted in segfaults (vaos were not available in 3.0)

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -39,6 +39,11 @@ namespace Hazel {
 			s_GLFWInitialised = true;
 		}
 
+		// set window hints, i.e. opengl version!
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		
 		m_Context = new OpenGLContext(m_Window);
