2b63b2be4213451863def60cbe1661f49453ae33
Svetlozar Georgiev <s.georgiev255@gmail.com>
2019-07-21 16:34:41 +0100

    fixed an issue where layers' OnAttach was being called twice which resulted in imgui-related crashes

--- Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/src/Hazel/LayerStack.cpp
@@ -15,12 +15,10 @@ namespace Hazel {
 	void LayerStack::PushLayer(Layer* layer) {
 		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
 		m_LayerInsertIndex++;
-		layer->OnAttach();
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay) {
 		m_Layers.emplace_back(overlay);
-		overlay->OnAttach();
 	}
 
 	void LayerStack::PopLayer(Layer* layer) {
