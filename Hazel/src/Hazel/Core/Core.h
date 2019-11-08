ed17967793872b4be1de8ea168b34962ce28a7fc
Svetlozar Georgiev <s.georgiev255@gmail.com>
2019-11-08 21:58:23 +0000

    Add automatic platform detection with predefined macros

--- Hazel/src/Hazel/Core/Core.h
+++ Hazel/src/Hazel/Core/Core.h
@@ -1,79 +1,119 @@
-// clang-format off
 #pragma once
 
 #include <memory>
 
-// Provide dynamic library options for Windows and Linux
-#if defined HZ_PLATFORM_WINDOWS
-	#if defined HZ_DYNAMIC_LINK
-		#if defined HZ_BUILD_DLL
-			#define HAZEL_API __declspec(dllexport)
-		#else
-			#define HAZEL_API __declspec(dllimport)
-		#endif
-	#else
-		#define HAZEL_API
-	#endif
-#elif defined HZ_PLATFORM_LINUX
-	#if defined HZ_DYNAMIC_LINK
-		#if defined HZ_BUILD_DLL
-			#define HAZEL_API __attribute__((visibility("default")))
-		#else
-			#define HAZEL_API
-		#endif
-	#else
-		#define HAZEL_API
-	#endif
-
+// Platform detection using predefined macros
+#ifdef _WIN32
+/* Windows x64/x86 */
+#	ifdef _WIN64
+/* Windows x64  */
+#		define HZ_PLATFORM_WINDOWS
+#	else
+/* Windows x86 */
+#		error "x86 builds are not supported!"
+#	endif
+#elif defined(__APPLE__) || defined(__MACH__)
+#	error "Platform APPLE is not supported!"
+/* We also have to check __ANDROID__ before __linux__
+ * since android is based on the linux kernel
+ * it has __linux__ defined */
+#elif defined(__ANDROID__)
+#	define HZ_PLATFORM_ANDROID
+#	error "Platform Android is not supported!"
+#elif defined(__linux__)
+#	define HZ_PLATFORM_LINUX
 #else
-	#error Unsupported platform!
+/* Unknown compiler/platform */
+#	error "Unknown platform!"
+#endif // End of platform detection
+
+// Provide dynamic library options for Windows and Linux.
+// This currently does *not* work as Hazel is linked statically
+// by default.
+#if defined HZ_PLATFORM_WINDOWS
+#	if defined HZ_DYNAMIC_LINK
+#		if defined HZ_BUILD_DLL
+#			define HAZEL_API __declspec(dllexport)
+#		else
+#			define HAZEL_API __declspec(dllimport)
+#		endif
+#	else
+#		define HAZEL_API
+#	endif
+#elif defined HZ_PLATFORM_LINUX
+#	if defined HZ_DYNAMIC_LINK
+#		if defined HZ_BUILD_DLL
+#			define HAZEL_API __attribute__((visibility("default")))
+#		else
+#			define HAZEL_API
+#		endif
+#	else
+#		define HAZEL_API
+#	endif
+#else
+#	error Unsupported platform!
 #endif
 
 // Debug settings
-#if defined HZ_DEBUG	
-	#if defined HZ_PLATFORM_WINDOWS
-		#define HZ_DEBUGBREAK() __debugbreak()
-	#elif defined HZ_PLATFORM_LINUX
-		#include <signal.h>
-		#define HZ_DEBUGBREAK() raise(SIGTRAP)
-	#endif
+#if defined HZ_DEBUG
+#	if defined HZ_PLATFORM_WINDOWS
+#		define HZ_DEBUGBREAK() __debugbreak()
+#	elif defined HZ_PLATFORM_LINUX
+#		include <signal.h>
+#		define HZ_DEBUGBREAK() raise(SIGTRAP)
+#	endif
 
-	#define HZ_ENABLE_RENDERER_LOG
-	#define HZ_ENABLE_ASSERTS
-	#define HZ_ENABLE_CORE_LOG
-	#define HZ_ENABLE_CLIENT_LOG
+#	define HZ_ENABLE_RENDERER_LOG
+#	define HZ_ENABLE_ASSERTS
+#	define HZ_ENABLE_CORE_LOG
+#	define HZ_ENABLE_CLIENT_LOG
 #endif // End of Debug settings
 
 // Assert statements
 #if defined HZ_ENABLE_ASSERTS
-	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); HZ_DEBUGBREAK(); } }
-	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); HZ_DEBUGBREAK(); } }
+#	define HZ_ASSERT(x, ...)                                                                      \
+		{                                                                                          \
+			if(!(x))                                                                               \
+			{                                                                                      \
+				HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__);                                    \
+				HZ_DEBUGBREAK();                                                                   \
+			}                                                                                      \
+		}
+#	define HZ_CORE_ASSERT(x, ...)                                                                 \
+		{                                                                                          \
+			if(!(x))                                                                               \
+			{                                                                                      \
+				HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__);                               \
+				HZ_DEBUGBREAK();                                                                   \
+			}                                                                                      \
+		}
 #else
-	#define HZ_ASSERT(x, ...)
-	#define HZ_CORE_ASSERT(x, ...)
-#endif 
+#	define HZ_ASSERT(x, ...)
+#	define HZ_CORE_ASSERT(x, ...)
+#endif
 
 // bit shifting macro
 #define BIT(x) (1 << x)
 
 #define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
 
-namespace Hazel {
+namespace Hazel
+{
 
-	template<typename T>
-	using Scope = std::unique_ptr<T>;
-	template<typename T, typename... Args>
-	constexpr Scope<T> MakeScope(Args&&... args)
-	{
-		return std::make_unique<T>(std::forward<Args>(args)...);
-	}
-
-	template<typename T>
-	using Ref = std::shared_ptr<T>;
-	template<typename T, typename... Args>
-	constexpr Ref<T> MakeRef(Args&&... args)
-	{
-		return std::make_shared<T>(std::forward<Args>(args)...);
-	}
-
+template <typename T>
+using Scope = std::unique_ptr<T>;
+template <typename T, typename... Args>
+constexpr Scope<T> MakeScope(Args&&... args)
+{
+	return std::make_unique<T>(std::forward<Args>(args)...);
 }
+
+template <typename T>
+using Ref = std::shared_ptr<T>;
+template <typename T, typename... Args>
+constexpr Ref<T> MakeRef(Args&&... args)
+{
+	return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+} // namespace Hazel
