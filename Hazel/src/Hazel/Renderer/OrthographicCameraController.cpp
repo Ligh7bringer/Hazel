1b5fac8ec9c3dced1576c078b30c9e8af10e4ac4
Svetlozar Georgiev <s.georgiev255@gmail.com>
2020-05-09 15:27:53 +0100

    OrthographicCameraController: Add a getter for screen bounds

--- Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
+++ Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
@@ -8,9 +8,11 @@ namespace Hazel
 
 OrthographicCameraController::OrthographicCameraController(float aspectRatio, bool rotation)
 	: m_AspectRatio(aspectRatio)
-	, m_Camera(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel)
+	, m_Camera(m_Bounds.Left, m_Bounds.Right, m_Bounds.Bottom, m_Bounds.Top)
+	, m_Bounds(
+		  {-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel})
 	, m_Rotation(rotation)
-{}
+{ }
 
 void OrthographicCameraController::OnUpdate(Timestep ts)
 {
@@ -73,8 +75,9 @@ bool OrthographicCameraController::OnMouseScrolled(MouseScrolledEvent& e)
 
 	m_ZoomLevel -= e.GetYOffset() * 0.25f;
 	m_ZoomLevel = std::max(m_ZoomLevel, 0.25f);
-	m_Camera.SetProjection(
-		-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+	m_Bounds = {
+		-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel};
+	m_Camera.SetProjection(m_Bounds.Left, m_Bounds.Right, m_Bounds.Bottom, m_Bounds.Top);
 	return false;
 }
 
@@ -83,8 +86,9 @@ bool OrthographicCameraController::OnWindowResized(WindowResizeEvent& e)
 	HZ_PROFILE_FUNCTION();
 
 	m_AspectRatio = (float)e.GetWidth() / (float)e.GetHeight();
-	m_Camera.SetProjection(
-		-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+	m_Bounds = {
+		-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel};
+	m_Camera.SetProjection(m_Bounds.Left, m_Bounds.Right, m_Bounds.Bottom, m_Bounds.Top);
 	return false;
 }
 
