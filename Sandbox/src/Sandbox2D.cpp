87b9a021b23be4057bdc5dd009767861b759a7e5
Svetlozar Georgiev <s.georgiev255@gmail.com>
2020-07-06 21:39:43 +0100

    SandboxApp: Fix imgui docking

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -127,75 +127,85 @@ void Sandbox2D::OnImGuiRender()
 {
 	HZ_PROFILE_FUNCTION();
 
-	static bool dockSpaceOpen = true;
-	static bool opt_fullscreen_persistant = true;
-	bool opt_fullscreen = opt_fullscreen_persistant;
-	static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-
-	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-	if(opt_fullscreen)
+	// Note: Switch this to true to enable dockspace
+	static bool dockingEnabled = true;
+	if(dockingEnabled)
 	{
-		ImGuiViewport* viewport = ImGui::GetMainViewport();
-		ImGui::SetNextWindowPos(viewport->GetWorkPos());
-		ImGui::SetNextWindowSize(viewport->GetWorkSize());
-		ImGui::SetNextWindowViewport(viewport->ID);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-						ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-	}
+		static bool dockspaceOpen = true;
+		static bool opt_fullscreen_persistant = true;
+		bool opt_fullscreen = opt_fullscreen_persistant;
+		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
 
-	if(dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-		window_flags |= ImGuiWindowFlags_NoBackground;
-
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-	ImGui::Begin("DockSpace Demo", &dockSpaceOpen, window_flags);
-	ImGui::PopStyleVar();
-
-	if(opt_fullscreen) ImGui::PopStyleVar(2);
-
-	ImGuiIO& io = ImGui::GetIO();
-	if(io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-	{
-		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-	}
-
-	if(ImGui::BeginMenuBar())
-	{
-		if(ImGui::BeginMenu("File"))
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+		if(opt_fullscreen)
 		{
-			if(ImGui::MenuItem("Exit", "")) Hazel::Application::Get().Close();
-			ImGui::EndMenu();
+			ImGuiViewport* viewport = ImGui::GetMainViewport();
+			ImGui::SetNextWindowPos(viewport->Pos);
+			ImGui::SetNextWindowSize(viewport->Size);
+			ImGui::SetNextWindowViewport(viewport->ID);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+			window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+							ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+			window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 		}
-		ImGui::EndMenuBar();
+
+		if(dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+			window_flags |= ImGuiWindowFlags_NoBackground;
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
+		ImGui::PopStyleVar();
+
+		if(opt_fullscreen) ImGui::PopStyleVar(2);
+
+		// DockSpace
+		ImGuiIO& io = ImGui::GetIO();
+		if(io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+		{
+			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+		}
+
+		if(ImGui::BeginMenuBar())
+		{
+			if(ImGui::BeginMenu("File"))
+			{
+				if(ImGui::MenuItem("Exit")) Hazel::Application::Get().Close();
+				ImGui::EndMenu();
+			}
+
+			ImGui::EndMenuBar();
+		}
+
+		ImGui::Begin("Performance");
+		ImGui::Text("%.3f ms/frame\n%.1f FPS",
+					1000.0f / ImGui::GetIO().Framerate,
+					ImGui::GetIO().Framerate);
+		ImGui::End();
+
+		ImGui::Begin("Settings");
+
+		auto stats = Hazel::Renderer2D::GetStats();
+		ImGui::Text("Renderer2D Stats:");
+		ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+		ImGui::Text("Quads: %d", stats.QuadCount);
+		ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+		ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
+		uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
+		ImGui::Image((void*)textureID, ImVec2{1280, 720});
+		ImGui::End();
+
+		ImGui::Begin("Renderer settings");
+		static int maxQuads = 20000;
+		ImGui::InputInt("Max quads per draw call", &maxQuads);
+		ImGui::InputInt("Quads to draw", &m_NumQuads);
+		Hazel::Renderer2D::SetMaxQuadsPerDrawCall(maxQuads);
+		ImGui::End();
+
+		ImGui::End();
 	}
-
-	ImGui::Begin("Performance");
-	ImGui::Text(
-		"%.3f ms/frame\n%.1f FPS", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-	ImGui::End();
-
-	ImGui::Begin("Settings");
-	static int maxQuads = 20000;
-	ImGui::InputInt("Max quads per draw call", &maxQuads);
-	ImGui::InputInt("Draw quads", &m_NumQuads);
-	Hazel::Renderer2D::SetMaxQuadsPerDrawCall(maxQuads);
-	ImGui::End();
-
-	auto stats = Hazel::Renderer2D::GetStats();
-	ImGui::Begin("Renderer2D Stats");
-	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
-	ImGui::Text("Quads: %d", stats.QuadCount);
-	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
-	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
-
-	uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
-	ImGui::Image((void*)textureID, ImVec2{1280.f, 720.f});
-	ImGui::End();
-
-	ImGui::End();
 }
 
 void Sandbox2D::OnEvent(Hazel::Event& event) { m_CameraController.OnEvent(event); }
