695eb0c60ff3c30aa81707dfb7e88f6904ba6db4
Svetlozar Georgiev <s.georgiev255@gmail.com>
2021-09-25 15:20:34 +0100

    GUI: Add thumbnails to content browser panel

--- Hazelnut/src/Panels/ContentBrowserPanel.cpp
+++ Hazelnut/src/Panels/ContentBrowserPanel.cpp
@@ -2,6 +2,7 @@
 
 #include <imgui.h>
 
+#include <algorithm>
 #include <string>
 
 namespace Hazel
@@ -11,7 +12,11 @@ static const std::filesystem::path s_AssetsPath{"assets"};
 
 ContentBrowserPanel::ContentBrowserPanel()
 	: m_CurrentDirectory(s_AssetsPath)
-{ }
+{
+	m_DirectoryIcon = Texture2D::Create("resources/icons/directory.png");
+	// FIXME: Load icon based on file extension
+	m_FileIcon = Texture2D::Create("resources/icons/file.png");
+}
 
 void ContentBrowserPanel::OnImGuiRender()
 {
@@ -25,20 +30,44 @@ void ContentBrowserPanel::OnImGuiRender()
 		}
 	}
 
+	static float padding = 16.f;
+	static float thumbnailSize = 128;
+	const float cellSize = thumbnailSize + padding;
+	const float panelWidth = ImGui::GetContentRegionAvail().x;
+	const int columnCount = std::max(static_cast<int>(panelWidth / cellSize), 1);
+	ImGui::Columns(columnCount, 0, false);
+
 	// FIXME: Don't iterate the filesystem every frame
 	for(auto& it : std::filesystem::directory_iterator(m_CurrentDirectory))
 	{
 		const auto& path = it.path();
 		auto relativePath = std::filesystem::relative(path, s_AssetsPath);
 		const std::string filename = relativePath.filename().string();
-		if(ImGui::Button(filename.c_str()))
+
+		const auto iconRendererId =
+			it.is_directory() ? m_DirectoryIcon->GetRendererID() : m_FileIcon->GetRendererID();
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+		ImGui::ImageButton(
+			(ImTextureID)iconRendererId, {thumbnailSize, thumbnailSize}, {0, 1}, {1, 0});
+		ImGui::PopStyleColor();
+		if(ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 		{
 			if(it.is_directory())
 			{
 				m_CurrentDirectory /= path.filename();
 			}
 		}
+
+		ImGui::TextWrapped("%s", filename.c_str());
+		ImGui::NextColumn();
 	}
+
+	ImGui::Columns(1);
+
+#if defined HZ_DEBUG
+	ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 512);
+	ImGui::SliderFloat("Padding", &padding, 0, 32);
+#endif
 
 	ImGui::End();
 }
--- Hazelnut/src/Panels/ContentBrowserPanel.hpp
+++ Hazelnut/src/Panels/ContentBrowserPanel.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Hazel/Renderer/Texture.hpp"
+
 #include <filesystem>
 
 namespace Hazel
@@ -7,8 +9,8 @@ namespace Hazel
 
 class ContentBrowserPanel
 {
-
 	std::filesystem::path m_CurrentDirectory;
+	Ref<Texture2D> m_DirectoryIcon, m_FileIcon;
 
 public:
 	ContentBrowserPanel();
